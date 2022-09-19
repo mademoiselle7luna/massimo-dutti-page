895eca52a6d28b265393dde40de3922abf5292e2
Kamila Świeżak <mademoiselle7luna@gmail.com>
2022-09-19 19:08:58 +0200

    add new section and fix alternative text

--- sass-utils/common.js
+++ sass-utils/common.js
@@ -1,10 +1,10 @@
 let mybutton = document.getElementById("topBtn");
 
-// When the user scrolls down 50px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -13,6 +13,7 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+

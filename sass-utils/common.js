3bb7eeb1cf31b9f2a1bae60b84effc3866b8e327
Kamila Świeżak <mademoiselle7luna@gmail.com>
2022-09-24 20:47:39 +0200

    Add new section, add new image and fix issues

--- sass-utils/common.js
+++ sass-utils/common.js
@@ -1,3 +1,5 @@
+import { gsap } from "gsap";
+
 let mybutton = document.getElementById("topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -16,4 +18,6 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
 
